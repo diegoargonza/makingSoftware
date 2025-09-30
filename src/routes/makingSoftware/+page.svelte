758bfc2aa5fc97e56a51b880f93120033673e103
diegoargonza <diegoargonza8@gmail.com>
2025-09-30 14:29:42 -0600

    Cambios 30/09/2025 cambio en la página

--- src/routes/makingSoftware/+page.svelte
+++ src/routes/makingSoftware/+page.svelte
@@ -1,10 +1,27 @@
 <script>
     import logo from '../../img/logoUNAM.png';
-    import imagen from '../../img/fondo-making.jpg'
     import logoMSoftware from "../../img/logoMSoftware.png";
+    import imgPrueba from '../../img/imgprueba.png';
 
 </script>
-<header class="p-2 bg-[url(./img/fondo-making.jpg)]">
+<header class="flex ">   
+    <div class="bg-[url(./img/fondo-making.jpg)] bg-cover w-1/2 ">
+        <div class="flex space-x-5 items-center">
+            <img src={logo} alt="logo-unam" class="h-10 w-30">
+            <div>
+                <p class="text-orange-700">Programa institucional de</p>
+                <p class="text-orange-700">Desarrollo de Software y Soluciones Tecnológicas</p>
+            </div>
+        </div>
+    </div>
+    <div class=" bg-[url(./img/Fondo2.jpg)] bg-cover w-1/2 ">
+        <div class="flex items-center justify-end z-10">
+            <img src={logoMSoftware} alt="logo-unam" class="h-10 w-30">
+        </div>
+    </div>
+</header>
+
+<!-- <header class="p-2 bg-[url(./img/fondo-making.jpg)]">
     <div class="flex justify-between items-center">
         <div class="flex  space-x-3 items-center">
             <img src={logo} alt="logo-unam" class=" h-10 w-30">
@@ -17,7 +34,7 @@
             <img src={logoMSoftware} alt="logo-unam" class=" h-10 w-30">
         </div>
     </div>
-</header>
+</header> -->
 
 <!-- Navegación -->
 
@@ -35,7 +52,7 @@
 <main class="bg-[url(./img/fondo-making.jpg),url(./img/logoUNAM.jpg)]  object-contain ">
  
 <div class="flex space-x-5 justify-center items-center p-20">
-    <img src="" alt="" class="border rounded-full p-2 w-50 h-50">
+    <img src={imgPrueba} alt="" class="rounded-full  w-50 h-50">
     <div class=" w-[600px]">
         <p class="text-xl">Si eres egresada o egresado de la UNAM, te invitamos a crear soluciones tecnológicas para nuestra universidad con las prácticas más utilizadas en el sector</p>
         <div class="mt-5 text-center">
@@ -48,28 +65,28 @@
 <div class="flex  space-x-5 justify-center">
     <div class="bg-slate-100 shadow-xl border border-slate-100 rounded-t-full rounded-bl-lg rounded-br-lg h-[400px] w-[230px] p-3">
         <div class="mt-2">
-            <img src="" alt="" class="border rounded-full p-2 w-50 h-50 mx-auto">
+            <img src={imgPrueba} alt="" class="rounded-full  w-50 h-50">
             <h2 class="text-center">Capacitación práctica</h2>
             <p>Aprende a desarrollar sistemas con enfoque ágil, ¡listo para la industria!</p>
         </div>
     </div>
 
     <div class=" bg-slate-100 border border-slate-100 shadow-xl rounded-t-full rounded-bl-lg  h-[400px] w-[230px] p-3" >
-        <img src="" alt="" class="border rounded-full p-2 w-50 h-50 mx-auto">
+        <img src={imgPrueba} alt="" class="rounded-full  w-50 h-50">
         <div class="mt-2">
             <h2 class="text-center">Experiencia Real</h2>
             <p>Colabora en proyectos multidiciplinarios que impactan directamente a la comunidad universitaria</p>
         </div>
     </div>
     <div class=" bg-slate-100 border border-slate-100 shadow-xl rounded-t-full  h-[400px] w-[230px] p-3 ">
-        <img src="" alt="" class="border rounded-full p-2 w-50 h-50 mx-auto">
+        <img src={imgPrueba} alt="" class="rounded-full  w-50 h-50">
         <div class="mt-2">
             <h2 class="text-center">Retribución económica</h2>
             <p>Gana mientras adquieres nuevas habilidades y creas soluciones tecnológicas de calidad</p>
         </div>
     </div>
     <div class=" bg-slate-100 border border-slate-100 shadow-xl rounded-t-full  h-[400px] w-[230px] p-3 ">
-        <img src="" alt="" class="border rounded-full p-2 w-50 h-50 mx-auto">
+        <img src={imgPrueba} alt="" class="rounded-full  w-50 h-50">
         <div class="mt-2">
             <h2 class="text-center ">Crecimiento profesional</h2>
             <p>Fortalece tus competencias trabajando en equipos, resolviendo desafíos reales y atumatizando procesos clave</p>
@@ -86,7 +103,7 @@
                 <h2 class="text-xl">Objetivos del programa </h2>
                 <p>Generar un programa de desarrollo de software y soluciones tecnológicas que ayude en la automatización de procesos institucionales universitarios con la aplicación de métodos formales de ingeniería de software, que contemplan la evaluación de laternativas como la adaptación, adquisición o desarrollo da la medida, con la participación de grupos de trabajo integrados por personal especialista y perfiles de egresados en formación, capacitados en prácticas formales adecuadas por la universidad para la construcción de software.</p>
             </div>
-            <img src="" alt="" class="border rounded-full p-2 w-50 h-50 mx-auto">
+           <img src={imgPrueba} alt="" class="rounded-full  w-50 h-50">
         </div>
     </div>
 </div>
