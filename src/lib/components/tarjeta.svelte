65315690cae4e48ad1d64f1f57e59951d8960f0c
diegoargonza <diegoargonza8@gmail.com>
2025-10-08 18:25:48 -0600

    Cambio en la forma de la tarjeta

--- src/lib/components/tarjeta.svelte
+++ src/lib/components/tarjeta.svelte
@@ -2,15 +2,18 @@
     let {titulo, texto, img} = $props();
     
 </script>
-
-<div class="h-[400px] w-[230px] p-3 border border-gray-300 bg-white rounded-t-full rounded-b-xl shadow-xl  ">
-    <div class="flex justify-center mp-10 items-center  " >
-        <div class="shadow-[-2px_-2px_3px_#fff,2px_2px_3px_#ccc] bg-rose-50/30 w-[200px] h-[200px] rounded-full ">
-            <img src={img} alt="" class="rounded-full  w-full h-full ">
+<div class="flex-none">
+    <div class="h-[230px] w-[230px] p-3  border-gray-300 bg-white rounded-t-full rounded-b-none shadow-xl border-x border-t ">
+        <div class="flex justify-center mp-10 items-center  " >
+            <div class="shadow-[-2px_-2px_3px_#fff,2px_2px_3px_#ccc] bg-rose-50/30 w-[200px] h-[200px] rounded-full ">
+                <img src={img} alt="" class="rounded-full  w-full h-full ">
+            </div>
         </div>
     </div>
-    <h2 class="text-center mt-2 text-sky-700 font-semibold">{titulo}</h2>
-    <p class="text-justify">{texto}</p>
+    <div class=" h-[200px] w-[230px]  bg-white p-2 rounded-b-xl border-x border-b border-gray-300">
+        <h2 class="text-center  text-sky-700 font-semibold">{titulo}</h2>
+        <p class="text-justify">{texto}</p>
+    </div>
 </div>
 
 <!-- 
