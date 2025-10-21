17a184bba903442061fbe7c0b3121ad95e034f7e
diego <diego@legolas.dcaa.unam.mx>
2025-10-21 13:05:21 -0600

    cambio en componente footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,10 +1,11 @@
 <script>
    import { base } from "$app/paths";
+   const anio = '2025';
 </script>
 
 <div class="text-center text-sky-800 bg-sepia-100   grid place-content-center p-10">
     <div class="">
-        <p>Hecho en México, Universidad Nacional Autónoma de México (UNAM), todos los derechos reservados 2025.</p>
+        <p>Hecho en México, Universidad Nacional Autónoma de México (UNAM), todos los derechos reservados {anio}.</p>
         <a class="underline" href="{base}/creditos">Créditos</a>
     </div>
 </div>
