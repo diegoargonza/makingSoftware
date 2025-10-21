f521f70d78d58917d76910538451d62ab935b042
diego <diego@legolas.dcaa.unam.mx>
2025-10-21 13:55:23 -0600

    fondo de footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -3,7 +3,7 @@
    const anio = '2025';
 </script>
 
-<div class="text-center text-sky-800 bg-sepia-100   grid place-content-center p-10">
+<div class="text-center text-sky-800  grid place-content-center p-10" id="seccion3">
     <div class="">
         <p>Hecho en México, Universidad Nacional Autónoma de México (UNAM), todos los derechos reservados {anio}.</p>
         <a class="underline" href="{base}/creditos">Créditos</a>
