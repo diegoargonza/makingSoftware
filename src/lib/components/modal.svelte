5e04da379f3262abfcdb766fec40db38c8ea5e0e
diegoargonza <diegoargonza8@gmail.com>
2025-10-20 18:35:54 -0600

    Ajustando modal

--- src/lib/components/modal.svelte
+++ src/lib/components/modal.svelte
@@ -13,12 +13,12 @@
     <el-dialog-backdrop class="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
     ></el-dialog-backdrop>
 
-    <div class="flex min-h-full items-end justify-center p-4 text-center focus:outline-none sm:items-center sm:p-0">
-      <el-dialog-panel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95">
-        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+    <div class="flex  items-end justify-center p-4 text-center focus:outline-none sm:items-center sm:p-0">
+      <el-dialog-panel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8  sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95">
+        <div class="bg-white px-20 pt-5 pb-4 sm:p-4 sm:pb-4">
           <div class="">
             <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-              <div class="flex items-center justify-between">
+              <div class="flex items-center justify-between gap-5">
                 <h3
                   id="dialog-title"
                   class="font-semibold text-sky-800 text-2xl"
